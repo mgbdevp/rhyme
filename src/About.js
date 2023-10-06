7380ecdae584158146a02f3c7fd2d9b7be152f6a
M <113066556+mgbdevp@users.noreply.github.com>
2023-10-06 21:14:29 +0400

    Update About.js

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ function About() {
     <div className ="App">
       <div className="About">
         <p style={{ marginBottom: 16 }}>Made by Magabee Using React And Spotify API</p>
-        <p><a href="#>Website</a></p>
+        <p><a href="#">Website</a></p>
         <p><a href="#">Github</a></p>
       </div>
       <p>Spotify API kurallarına uygun olarak, siteden direkt olarak şarkının sadece 30 saniyelik önizlemesini çalabilirsiniz.</p>
